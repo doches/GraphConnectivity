f2ded4bbb6f168a669699b2c164e0a3b5647b681
Trevor Fountain <doches@gmail.com>
2012-01-23 14:05:30 +0000

    Pick 10 random exemplars instead of all.

--- bin/yaml2subset.rb
+++ bin/yaml2subset.rb
@@ -1,6 +1,9 @@
+#!/usr/bin/env ruby
 # Takes a yamlized list of category:exemplars and extracts a subset of categories.
 #
 # Usage: ruby #{$0} file.yaml category1 category2 ...
+
+Exemplars_Per_Category = 10
 
 require 'rubygems'
 require 'wordnet'
@@ -14,5 +17,9 @@ while not ARGV.empty?
 end
 
 subset = gold.reject { |category,exemplars| not categories.include?(category) }
+
+subset.each_pair do |category, exemplars|
+  subset[category] = exemplars.shuffle[0..Exemplars_Per_Category-1] if exemplars.size > Exemplars_Per_Category
+end
 
 puts subset.to_yaml
