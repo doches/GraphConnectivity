ff2af0a228954837a0b4ca566d3ec735967e00e1
Trevor Fountain <doches@gmail.com>
2012-01-24 11:52:53 +0000

    Use progressbar; normalise against the highest found degree

--- lib/measures/Degree.rb
+++ lib/measures/Degree.rb
@@ -2,21 +2,33 @@ module Degree
   def degree(node)
     @degree_cache ||= {}
     @degree_cache[node] ||= edges_from(node).values.inject(0) { |s,x| s += x }
+    @highest_degree = [node, @degree_cache[node]] if @highest_degree.nil? or @degree_cache[node] > @highest_degree[1]
     
     return @degree_cache[node]
   end
   
   def reweight!
     new_edges = {}
-    progress = ProgressBar.new("reweighting",@edges.size**2) if @use_progressbar
+    
+    # Fill in degree cache for nodes
+    progress = ProgressBar.new("reweighting",@edges.size + @edges.size**2) if @use_progressbar
+    @edges.keys.each do |from| 
+      degree(from)
+      progress.inc if @use_progressbar
+    end
+    
+    # Compute edge degrees
     @edges.each_pair do |from, list|
       new_edges[from] ||= {}
       list.each_pair do |to, weight|
         new_edges[from][to] = (degree(from) + degree(to))/2.0
+        # Normalise against highest possible degree
+        new_edges[from][to] /= @highest_degree[1]
         progress.inc if @use_progressbar
       end
     end
     progress.finish if @use_progressbar
+    
     @edges = new_edges
   end
 end
