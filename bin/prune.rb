25e6a8588a5f7b78f743be2691cb888c3f21a936
Trevor Fountain <doches@gmail.com>
2012-01-24 11:03:17 +0000

    Added global (old-style) thresholding; don't require wordmap on cl

--- bin/prune.rb
+++ bin/prune.rb
@@ -7,17 +7,10 @@ require 'lib/Wordmap'
 require 'lib/Graph'
 require 'optparse'
 
-graph_f = ARGV.shift
-wordmap_f = graph_f.gsub(/graph$/,"wordmap")
-
-wordmap = Wordmap.new(wordmap_f)
-graph = Graph.new(graph_f, wordmap)
-
 known_prunings = [:mean, :median]
-known_formats = [:dot, :graph, :human_graph]
 
 # Default options
-options = {:method => :none, :output => :dot}
+options = {:method => :none, :output => :dot, :threshold => -1.0}
 
 # Parse command-line options
 parser = OptionParser.new do |opts|
@@ -34,44 +27,45 @@ parser = OptionParser.new do |opts|
           "Use pruning method (#{known_prunings.join(', ')}; defaults to #{options[:method]})") do |m|
     options[:method] = m
   end
+  # Implement thresholding
+  opts.on("-t THRESHOLD", 
+          "--threshold THRESHOLD", 
+          "Only output edges above a specified weight; disabled by default") do |t|
+    options[:threshold] = t.to_f
+  end
 
   # Specify the output format (dot, graph, human-readable graph)
   opts.on("-o FORMAT",
           "--output FORMAT",
-          known_formats,
-          "Output re-weighted graph as (#{known_formats.join(', ')}; defaults to #{options[:output]})") do |format|
+          Graph.formats,
+          "Output re-weighted graph as (#{Graph.formats.join(', ')}; defaults to #{options[:output]})") do |format|
     options[:output] = format
   end
-end.parse!
-
-if options[:method] == :none
-  STDERR.puts "Pruning method required!\n"
-  STDERR.puts parser
-  exit(1)
 end
 
-# Build a module/file name from the prune method, and inject it into Graph
-klass = options[:method].to_s.split("_").map { |x| x.capitalize }.join("")
-file = File.join("lib","prunings",klass)
-require(file)
-mixin = <<RUB
+parser.parse!
+
+graph_f = ARGV.shift
+
+graph = Graph.new(graph_f)
+graph.threshold = options[:threshold]
+wordmap = graph.wordmap
+
+if options[:method] != :none
+  # Build a module/file name from the prune method, and inject it into Graph
+  klass = options[:method].to_s.split("_").map { |x| x.capitalize }.join("")
+  file = File.join("lib","prunings",klass)
+  require(file)
+  mixin = <<RUB
 class Graph
   include #{klass}
 end
 RUB
-eval(mixin)
+  eval(mixin)
 
-# Prune! 
-graph.prune!
+  # Prune! 
+  graph.prune!
+end
 
 # Output in specified format
-case options[:output]
-  when :dot
-    puts graph.to_dot
-  when :human_graph
-    puts graph.to_s(true)
-  when :graph
-    puts graph.to_s
-  else
-    STDERR.puts "Unknown graph output format \"#{options[:output]}\" specified."
-end
+puts graph.format(options[:output])
